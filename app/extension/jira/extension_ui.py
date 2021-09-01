46efb787ca8a4e8968e5722b94539933572bf414
Remie Bolte <remie@collabsoft.net>
2021-09-01 16:06:52 +0200

    Add selenium tests

--- app/extension/jira/extension_ui.py
+++ app/extension/jira/extension_ui.py
@@ -17,42 +17,36 @@ def app_specific_action(webdriver, datasets):
         @print_timing("selenium_app_custom_action:view_vcs_page")
         def sub_measure():
             page.go_to_url(f"{JIRA_SETTINGS.server_url}/secure/VCSPage.jspa?s=projects&pid={project_id}")
-            page.wait_for_page_loaded()
             page.wait_until_visible((By.CSS_SELECTOR, "iframe[data-ac-polyfill]"))  # Wait for you app-specific UI element by ID selector
         sub_measure()
 
         @print_timing("selenium_app_custom_action:view_vcs_overview")
         def sub_measure():
             page.go_to_url(f"{JIRA_SETTINGS.server_url}/secure/VCSAdminPage.jspa?s=overview")
-            page.wait_for_page_loaded()
             page.wait_until_visible((By.CSS_SELECTOR, "iframe[data-ac-polyfill]"))  # Wait for you app-specific UI element by ID selector
         sub_measure()
 
         @print_timing("selenium_app_custom_action:view_vcs_settings")
         def sub_measure():
             page.go_to_url(f"{JIRA_SETTINGS.server_url}/secure/VCSAdminPage.jspa?s=settings")
-            page.wait_for_page_loaded()
             page.wait_until_visible((By.CSS_SELECTOR, "iframe[data-ac-polyfill]"))  # Wait for you app-specific UI element by ID selector
         sub_measure()
 
         @print_timing("selenium_app_custom_action:view_vcs_conflicts")
         def sub_measure():
             page.go_to_url(f"{JIRA_SETTINGS.server_url}/secure/VCSAdminPage.jspa?s=conflicts")
-            page.wait_for_page_loaded()
             page.wait_until_visible((By.CSS_SELECTOR, "iframe[data-ac-polyfill]"))  # Wait for you app-specific UI element by ID selector
         sub_measure()
 
         @print_timing("selenium_app_custom_action:view_vcs_audit_log")
         def sub_measure():
             page.go_to_url(f"{JIRA_SETTINGS.server_url}/secure/VCSAdminPage.jspa?s=auditLog")
-            page.wait_for_page_loaded()
             page.wait_until_visible((By.CSS_SELECTOR, "iframe[data-ac-polyfill]"))  # Wait for you app-specific UI element by ID selector
         sub_measure()
 
         @print_timing("selenium_app_custom_action:view_vcs_task_manager")
         def sub_measure():
             page.go_to_url(f"{JIRA_SETTINGS.server_url}/secure/VCSAdminPage.jspa?s=taskManager")
-            page.wait_for_page_loaded()
             page.wait_until_visible((By.CSS_SELECTOR, "iframe[data-ac-polyfill]"))  # Wait for you app-specific UI element by ID selector
         sub_measure()
     measure()
