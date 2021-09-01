f5370a39c3afc32546bfe42326ed8c44ecd3608e
Remie Bolte <remie@collabsoft.net>
2021-09-01 16:20:57 +0200

    Add selenium tests

--- app/extension/jira/extension_ui.py
+++ app/extension/jira/extension_ui.py
@@ -11,6 +11,21 @@ def app_specific_action(webdriver, datasets):
     project = random.choice(datasets['projects'])
     project_id = project[1]
 
+    @print_timing("selenium_app_specific_user_login")
+    def measure():
+        def app_specific_user_login(username='admin', password='admin'):
+            login_page = Login(webdriver)
+            login_page.delete_all_cookies()
+            login_page.go_to()
+            login_page.set_credentials(username=username, password=password)
+            if login_page.is_first_login():
+                login_page.first_login_setup()
+            if login_page.is_first_login_second_page():
+                login_page.first_login_second_page_setup()
+            login_page.wait_for_page_loaded()
+        app_specific_user_login(username='admin', password='admin')
+    measure()
+
     @print_timing("selenium_app_custom_action")
     def measure():
 
