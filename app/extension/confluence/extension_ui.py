9e58b19262a29e02b8a44ec7b066544e07146d26
Remie Bolte <remie@collabsoft.net>
2021-12-14 20:39:36 +0100

    Add scale & performance tests

--- app/extension/confluence/extension_ui.py
+++ app/extension/confluence/extension_ui.py
@@ -62,168 +62,168 @@ def app_specific_action(webdriver, datasets):
         def sub_measure():
             page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/pages/viewpage.action?pageId={adobepdf_viewer_specific_page_id}")
             page.wait_until_visible((By.ID, "title-text"))  # Wait for title field visible
-            page.wait_until_visible((By.CSS_SELECTOR, "iframe.ac-content"))  # Wait for you app-specific UI element by ID selector
+            page.wait_until_visible((By.CSS_SELECTOR, 'iframe[src^="https://embedder.app"]'))  # Wait for you app-specific UI element by ID selector
         sub_measure()
 
         @print_timing("selenium_app_custom_action:view_macro_page:adobepdf_slideshow")
         def sub_measure():
             page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/pages/viewpage.action?pageId={adobepdf_slideshow_specific_page_id}")
             page.wait_until_visible((By.ID, "title-text"))  # Wait for title field visible
-            page.wait_until_visible((By.CSS_SELECTOR, "iframe.ac-content"))  # Wait for you app-specific UI element by ID selector
+            page.wait_until_visible((By.CSS_SELECTOR, 'iframe[src^="https://embedder.app"]'))  # Wait for you app-specific UI element by ID selector
         sub_measure()
 
         @print_timing("selenium_app_custom_action:view_macro_page:adobepdf_inline")
         def sub_measure():
             page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/pages/viewpage.action?pageId={adobepdf_inline_specific_page_id}")
             page.wait_until_visible((By.ID, "title-text"))  # Wait for title field visible
-            page.wait_until_visible((By.CSS_SELECTOR, "iframe.ac-content"))  # Wait for you app-specific UI element by ID selector
+            page.wait_until_visible((By.CSS_SELECTOR, 'iframe[src^="https://embedder.app"]'))  # Wait for you app-specific UI element by ID selector
         sub_measure()
 
         @print_timing("selenium_app_custom_action:view_macro_page:adobexd")
         def sub_measure():
             page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/pages/viewpage.action?pageId={adobexd_specific_page_id}")
             page.wait_until_visible((By.ID, "title-text"))  # Wait for title field visible
-            page.wait_until_visible((By.CSS_SELECTOR, "iframe.ac-content"))  # Wait for you app-specific UI element by ID selector
+            page.wait_until_visible((By.CSS_SELECTOR, 'iframe[src^="https://embedder.app"]'))  # Wait for you app-specific UI element by ID selector
         sub_measure()
 
         @print_timing("selenium_app_custom_action:view_macro_page:airtable")
         def sub_measure():
             page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/pages/viewpage.action?pageId={airtable_specific_page_id}")
             page.wait_until_visible((By.ID, "title-text"))  # Wait for title field visible
-            page.wait_until_visible((By.CSS_SELECTOR, "iframe.ac-content"))  # Wait for you app-specific UI element by ID selector
+            page.wait_until_visible((By.CSS_SELECTOR, 'iframe[src^="https://embedder.app"]'))  # Wait for you app-specific UI element by ID selector
         sub_measure()
 
         @print_timing("selenium_app_custom_action:view_macro_page:calendly")
         def sub_measure():
             page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/pages/viewpage.action?pageId={calendly_specific_page_id}")
             page.wait_until_visible((By.ID, "title-text"))  # Wait for title field visible
-            page.wait_until_visible((By.CSS_SELECTOR, "iframe.ac-content"))  # Wait for you app-specific UI element by ID selector
+            page.wait_until_visible((By.CSS_SELECTOR, 'iframe[src^="https://embedder.app"]'))  # Wait for you app-specific UI element by ID selector
         sub_measure()
 
         @print_timing("selenium_app_custom_action:view_macro_page:canva")
         def sub_measure():
             page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/pages/viewpage.action?pageId={canva_specific_page_id}")
             page.wait_until_visible((By.ID, "title-text"))  # Wait for title field visible
-            page.wait_until_visible((By.CSS_SELECTOR, "iframe.ac-content"))  # Wait for you app-specific UI element by ID selector
+            page.wait_until_visible((By.CSS_SELECTOR, 'iframe[src^="https://embedder.app"]'))  # Wait for you app-specific UI element by ID selector
         sub_measure()
 
         @print_timing("selenium_app_custom_action:view_macro_page:deepnote")
         def sub_measure():
             page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/pages/viewpage.action?pageId={deepnote_specific_page_id}")
             page.wait_until_visible((By.ID, "title-text"))  # Wait for title field visible
-            page.wait_until_visible((By.CSS_SELECTOR, "iframe.ac-content"))  # Wait for you app-specific UI element by ID selector
+            page.wait_until_visible((By.CSS_SELECTOR, 'iframe[src^="https://embedder.app"]'))  # Wait for you app-specific UI element by ID selector
         sub_measure()
 
         @print_timing("selenium_app_custom_action:view_macro_page:figma")
         def sub_measure():
             page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/pages/viewpage.action?pageId={figma_specific_page_id}")
             page.wait_until_visible((By.ID, "title-text"))  # Wait for title field visible
-            page.wait_until_visible((By.CSS_SELECTOR, "iframe.ac-content"))  # Wait for you app-specific UI element by ID selector
+            page.wait_until_visible((By.CSS_SELECTOR, 'iframe[src^="https://embedder.app"]'))  # Wait for you app-specific UI element by ID selector
         sub_measure()
 
         @print_timing("selenium_app_custom_action:view_macro_page:framer")
         def sub_measure():
             page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/pages/viewpage.action?pageId={framer_specific_page_id}")
             page.wait_until_visible((By.ID, "title-text"))  # Wait for title field visible
-            page.wait_until_visible((By.CSS_SELECTOR, "iframe.ac-content"))  # Wait for you app-specific UI element by ID selector
+            page.wait_until_visible((By.CSS_SELECTOR, 'iframe[src^="https://embedder.app"]'))  # Wait for you app-specific UI element by ID selector
         sub_measure()
 
         @print_timing("selenium_app_custom_action:view_macro_page:geckoboard")
         def sub_measure():
             page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/pages/viewpage.action?pageId={geckoboard_specific_page_id}")
             page.wait_until_visible((By.ID, "title-text"))  # Wait for title field visible
-            page.wait_until_visible((By.CSS_SELECTOR, "iframe.ac-content"))  # Wait for you app-specific UI element by ID selector
+            page.wait_until_visible((By.CSS_SELECTOR, 'iframe[src^="https://embedder.app"]'))  # Wait for you app-specific UI element by ID selector
         sub_measure()
 
         @print_timing("selenium_app_custom_action:view_macro_page:github")
         def sub_measure():
             page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/pages/viewpage.action?pageId={github_specific_page_id}")
             page.wait_until_visible((By.ID, "title-text"))  # Wait for title field visible
-            page.wait_until_visible((By.CSS_SELECTOR, "iframe.ac-content"))  # Wait for you app-specific UI element by ID selector
+            page.wait_until_visible((By.CSS_SELECTOR, 'iframe[src^="https://embedder.app"]'))  # Wait for you app-specific UI element by ID selector
         sub_measure()
 
         @print_timing("selenium_app_custom_action:view_macro_page:gslides")
         def sub_measure():
             page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/pages/viewpage.action?pageId={gslides_specific_page_id}")
             page.wait_until_visible((By.ID, "title-text"))  # Wait for title field visible
-            page.wait_until_visible((By.CSS_SELECTOR, "iframe.ac-content"))  # Wait for you app-specific UI element by ID selector
+            page.wait_until_visible((By.CSS_SELECTOR, 'iframe[src^="https://embedder.app"]'))  # Wait for you app-specific UI element by ID selector
         sub_measure()
 
         @print_timing("selenium_app_custom_action:view_macro_page:hotjar")
         def sub_measure():
             page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/pages/viewpage.action?pageId={hotjar_specific_page_id}")
             page.wait_until_visible((By.ID, "title-text"))  # Wait for title field visible
-            page.wait_until_visible((By.CSS_SELECTOR, "iframe.ac-content"))  # Wait for you app-specific UI element by ID selector
+            page.wait_until_visible((By.CSS_SELECTOR, 'iframe[src^="https://embedder.app"]'))  # Wait for you app-specific UI element by ID selector
         sub_measure()
 
         @print_timing("selenium_app_custom_action:view_macro_page:invision")
         def sub_measure():
             page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/pages/viewpage.action?pageId={invision_specific_page_id}")
             page.wait_until_visible((By.ID, "title-text"))  # Wait for title field visible
-            page.wait_until_visible((By.CSS_SELECTOR, "iframe.ac-content"))  # Wait for you app-specific UI element by ID selector
+            page.wait_until_visible((By.CSS_SELECTOR, 'iframe[src^="https://embedder.app"]'))  # Wait for you app-specific UI element by ID selector
         sub_measure()
 
         @print_timing("selenium_app_custom_action:view_macro_page:keynote")
         def sub_measure():
             page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/pages/viewpage.action?pageId={keynote_specific_page_id}")
             page.wait_until_visible((By.ID, "title-text"))  # Wait for title field visible
-            page.wait_until_visible((By.CSS_SELECTOR, "iframe.ac-content"))  # Wait for you app-specific UI element by ID selector
+            page.wait_until_visible((By.CSS_SELECTOR, 'iframe[src^="https://embedder.app"]'))  # Wait for you app-specific UI element by ID selector
         sub_measure()
 
         @print_timing("selenium_app_custom_action:view_macro_page:marvel")
         def sub_measure():
             page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/pages/viewpage.action?pageId={marvel_specific_page_id}")
             page.wait_until_visible((By.ID, "title-text"))  # Wait for title field visible
-            page.wait_until_visible((By.CSS_SELECTOR, "iframe.ac-content"))  # Wait for you app-specific UI element by ID selector
+            page.wait_until_visible((By.CSS_SELECTOR, 'iframe[src^="https://embedder.app"]'))  # Wait for you app-specific UI element by ID selector
         sub_measure()
 
         @print_timing("selenium_app_custom_action:view_macro_page:miro")
         def sub_measure():
             page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/pages/viewpage.action?pageId={miro_specific_page_id}")
             page.wait_until_visible((By.ID, "title-text"))  # Wait for title field visible
-            page.wait_until_visible((By.CSS_SELECTOR, "iframe.ac-content"))  # Wait for you app-specific UI element by ID selector
+            page.wait_until_visible((By.CSS_SELECTOR, 'iframe[src^="https://embedder.app"]'))  # Wait for you app-specific UI element by ID selector
         sub_measure()
 
         @print_timing("selenium_app_custom_action:view_macro_page:netlify")
         def sub_measure():
             page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/pages/viewpage.action?pageId={netlify_specific_page_id}")
             page.wait_until_visible((By.ID, "title-text"))  # Wait for title field visible
-            page.wait_until_visible((By.CSS_SELECTOR, "iframe.ac-content"))  # Wait for you app-specific UI element by ID selector
+            page.wait_until_visible((By.CSS_SELECTOR, 'iframe[src^="https://embedder.app"]'))  # Wait for you app-specific UI element by ID selector
         sub_measure()
 
         @print_timing("selenium_app_custom_action:view_macro_page:octopus")
         def sub_measure():
             page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/pages/viewpage.action?pageId={octopus_specific_page_id}")
             page.wait_until_visible((By.ID, "title-text"))  # Wait for title field visible
-            page.wait_until_visible((By.CSS_SELECTOR, "iframe.ac-content"))  # Wait for you app-specific UI element by ID selector
+            page.wait_until_visible((By.CSS_SELECTOR, 'iframe[src^="https://embedder.app"]'))  # Wait for you app-specific UI element by ID selector
         sub_measure()
 
         @print_timing("selenium_app_custom_action:view_macro_page:pitch")
         def sub_measure():
             page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/pages/viewpage.action?pageId={pitch_specific_page_id}")
             page.wait_until_visible((By.ID, "title-text"))  # Wait for title field visible
-            page.wait_until_visible((By.CSS_SELECTOR, "iframe.ac-content"))  # Wait for you app-specific UI element by ID selector
+            page.wait_until_visible((By.CSS_SELECTOR, 'iframe[src^="https://embedder.app"]'))  # Wait for you app-specific UI element by ID selector
         sub_measure()
 
         @print_timing("selenium_app_custom_action:view_macro_page:prezi")
         def sub_measure():
             page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/pages/viewpage.action?pageId={prezi_specific_page_id}")
             page.wait_until_visible((By.ID, "title-text"))  # Wait for title field visible
-            page.wait_until_visible((By.CSS_SELECTOR, "iframe.ac-content"))  # Wait for you app-specific UI element by ID selector
+            page.wait_until_visible((By.CSS_SELECTOR, 'iframe[src^="https://embedder.app"]'))  # Wait for you app-specific UI element by ID selector
         sub_measure()
 
         @print_timing("selenium_app_custom_action:view_macro_page:protopie")
         def sub_measure():
             page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/pages/viewpage.action?pageId={protopie_specific_page_id}")
             page.wait_until_visible((By.ID, "title-text"))  # Wait for title field visible
-            page.wait_until_visible((By.CSS_SELECTOR, "iframe.ac-content"))  # Wait for you app-specific UI element by ID selector
+            page.wait_until_visible((By.CSS_SELECTOR, 'iframe[src^="https://embedder.app"]'))  # Wait for you app-specific UI element by ID selector
         sub_measure()
 
         @print_timing("selenium_app_custom_action:view_macro_page:zeplin")
         def sub_measure():
             page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/pages/viewpage.action?pageId={zeplin_specific_page_id}")
             page.wait_until_visible((By.ID, "title-text"))  # Wait for title field visible
-            page.wait_until_visible((By.CSS_SELECTOR, "iframe.ac-content"))  # Wait for you app-specific UI element by ID selector
+            page.wait_until_visible((By.CSS_SELECTOR, 'iframe[src^="https://embedder.app"]'))  # Wait for you app-specific UI element by ID selector
         sub_measure()
 
     measure()
